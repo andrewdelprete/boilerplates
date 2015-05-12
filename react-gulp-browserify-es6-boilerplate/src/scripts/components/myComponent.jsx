c976b6decbcbafbd104f4bda341c64be75f2caa0
Andrew Del Prete <andrew@musicbed.com>
2015-05-11 21:22:49 -0800

    Removed state from boilerplate before anyone sees it. State == Evil :D

--- react-gulp-browserify-es6-boilerplate/src/scripts/components/myComponent.jsx
+++ react-gulp-browserify-es6-boilerplate/src/scripts/components/myComponent.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 class MyComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            title: props.title
-        }
     }
 
     render() {
         return (
             <div>
-                <h1>{ this.state.title }</h1>
+                <h1>{ this.props.title }</h1>
             </div>
         );
     }
