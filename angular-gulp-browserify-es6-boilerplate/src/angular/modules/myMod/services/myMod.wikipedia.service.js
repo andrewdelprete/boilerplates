c7c96ee7324fdcd3473b3bec06406aadedebfdec
Andrew Del Prete <andrew@musicbed.com>
2015-05-19 07:25:30 -0800

    Added inline annotation to service to inject as an array for minification

--- angular-gulp-browserify-es6-boilerplate/src/angular/modules/myMod/services/myMod.wikipedia.service.js
+++ angular-gulp-browserify-es6-boilerplate/src/angular/modules/myMod/services/myMod.wikipedia.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var service = function($http) {
+var service = ['$http', function($http) {
 
     /**
      * Search Wikipedia for specified term
@@ -10,6 +10,6 @@ var service = function($http) {
     this.search = (term) => {
         return $http.jsonp('http://en.wikipedia.org/w/api.php?&search=' + term.toLowerCase() + '&action=opensearch&format=json&callback=JSON_CALLBACK');
     };
-};
+}];
 
 export default service;
