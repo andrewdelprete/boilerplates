edb549c9123dadd2a3c26a69d7c24ef85ecf870d
Andrew Del Prete <andrew@musicbed.com>
2015-10-29 08:19:52 -0800

    Updated npm packages

--- react-immutablejs-gulp-browserify-es6-boilerplate/src/scripts/app.jsx
+++ react-immutablejs-gulp-browserify-es6-boilerplate/src/scripts/app.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Immutable from 'immutable';
 import { MyComponent } from './components';
 
 const data = Immutable.fromJS({ title: "My Component" });
 
-React.render(<MyComponent data={ data } />, document.getElementById('app'));
-
-
+ReactDOM.render(<MyComponent data={ data } />, document.getElementById('app'));
